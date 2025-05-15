53bc4713d959ecdfc054f76da545573d2191bff5
rfkhrmwn <moontonakun481@gmail.com>
2025-05-15 13:31:19 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc' 
-global.apikey = ["Zeeoneofc", "Alphabot", "Alpha"]
+global.creator = 'rfkhrmwn' 
+global.apikey = ["rfkhrmwn03", "susi", "anggun"]
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
